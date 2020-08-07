e18cfb8f3ac0f783f0e9175d1ce5cc493e7dcbfd
John McNay <john.w.mcnay@gmail.com>
2020-08-07 13:14:38 -0500

    added displaying nameAsText() and its supporting functions

--- js/deck.js
+++ js/deck.js
@@ -11,14 +11,47 @@ class playingCard {
     belongsTo = {};
     currentLocation = {};
 
-    constructor(){
-
+    constructor(rank, suit){
+        this.rank = rank;
+        this.suit = suit;
     }
 
     //turns the card over, toggling isVisible and returning the new value
     flip() {
         this.isVisible = !this.isVisible;
         return this.isVisible;
+    }
+
+    rankAsText() {
+        switch (this.rank) {
+            case "J":
+                return "Jack";
+            case "Q":
+                return "Queen";
+            case "K":
+                return "King";
+            case "A":
+                return "Ace";
+            default:
+                return this.rank;
+        }
+    }
+
+    suitAsText() {
+        switch (this.suit) {
+            case "H":
+                return "Hearts";
+            case "S":
+                return "Spades";
+            case "C":
+                return "Clubs";
+            case "D":
+                return "Diamonds";
+        }
+    }
+
+    nameAsText() {
+        return this.rankAsText() + " of " + this.suitAsText();
     }
 
     //moveTo moves card to a new location
@@ -38,22 +71,27 @@ class playingCardDeck {
         this.createDeck()
     }
 
+    //returns an array of playingCard objects
     defaultDeck() {
         let cardArray = [];
         for (var suit of ['H', 'S', 'D', 'C']) {
             for (var rank of ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']) {
-                cardArray.push(suit + rank);
+                cardArray.push(new playingCard(rank, suit));
             }
         }
         return cardArray;
     }
-    //createDeck takes in a function that stores and returns an array of playingCards
+    //createDeck takes in a function that stores and stores an array of playingCards
     createDeck(createFunction=this.defaultDeck) {
         this.cards = createFunction();
     }
 
+    cardsInDeck() {
+        return this.cards.length;
+    }
 
-
+    //removeCard
+    //addCard
     //shuffle() randomizes deck
 
 }
