e3162a4c105d74087f32d48fcf10f8a8fc3af8f0
John McNay <john.w.mcnay@gmail.com>
2020-08-07 12:54:43 -0500

    basic deck creation

--- js/deck.js
+++ js/deck.js
@@ -1,1 +1,63 @@
 
+//cards have a suit, and rank, and sometimes an alternate value (i.e. wild cards, Ace 1 or 11)
+class playingCard {
+
+    suit = ''; //possible suits H S D C
+    rank = ''; //2 3 4 5 6 7 8 9 10 J K Q A Joker
+    value = 0;
+    altValue = 0;
+    isVisible = false;
+    isWild = false;
+    belongsTo = {};
+    currentLocation = {};
+
+    constructor(){
+
+    }
+
+    //turns the card over, toggling isVisible and returning the new value
+    flip() {
+        this.isVisible = !this.isVisible;
+        return this.isVisible;
+    }
+
+    //moveTo moves card to a new location
+    //randomize, generate a random card
+    //createCard
+
+}
+
+
+
+
+class playingCardDeck {
+
+    cards = []; //an array of playingCard objects
+
+    constructor() {
+        this.createDeck()
+    }
+
+    defaultDeck() {
+        let cardArray = [];
+        for (var suit of ['H', 'S', 'D', 'C']) {
+            for (var rank of ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']) {
+                cardArray.push(suit + rank);
+            }
+        }
+        return cardArray;
+    }
+    //createDeck takes in a function that stores and returns an array of playingCards
+    createDeck(createFunction=this.defaultDeck) {
+        this.cards = createFunction();
+    }
+
+
+
+    //shuffle() randomizes deck
+
+}
+
+//cardTable = {} object for a card table
+//seats/players
+//"dealer"
--- tests.js
+++ tests.js
@@ -6,3 +6,40 @@
 //
 //     });
 // });
+
+describe("playingCard", function() {
+
+    it("class should be defined", function () {
+
+        expect(typeof playingCard).toBe("function");
+
+    });
+    it("creating a new instance should be type object", function () {
+
+        expect(typeof(new playingCard())).toBe("object");
+    });
+
+    it("flip() should toggle isVisible, storing and also returning the value", function () {
+
+        expect((function () {
+            var card = new playingCard();
+            var previousVisibility = card.isVisible;
+            var flipResult = card.flip();
+
+            return ((flipResult === card.isVisible) && (card.isVisible !== previousVisibility));
+        })()).toBe(true);
+
+    });
+
+
+});
+
+
+describe("playingCardDeck", function() {
+
+    it("class should type function", function () {
+
+        expect(typeof playingCardDeck).toBe("function");
+
+    });
+});
